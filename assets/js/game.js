f899b5c69c46614e91fc009f56da5086e526d3ea
rmisu <roger889@umn.edu>
2022-06-09 05:05:31 -0500

    first round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,47 @@
 var playerName = window.prompt("What is your robot's name?")
-// Note the lack of quotation marks around playerName
-console.log("Our robot's name is" + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+
+//mutiple values can be logged at once
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 // this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
-// fight();
+var fight = function() {
+    //alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    //subtract the value of playerAttack from enemyHealth and use that result to update enemyHealth variable
+    enemyHealth = enemyHealth - playerAttack;
+    //log resulting message to console
+    console.log(
+        playerName + " attacked" + enemyName + ". " + enemyName + "now has" + enemyHealth + "health remaining."
+    );
+    //check enemyHealth
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    //subtract the value of enemyAttack from playerHealth and use that result to update playerHealth variable
+    playerHealth = playerHealth - enemyAttack;
+    //log resulting message to console
+    console.log(
+        enemyName + " attacked" + playerName + ". " + playerName + " now has" + playerHealth + "health remaining."
+    )
+    //check playerHealth
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+};
+// executes function "fight"
+fight();
 
 
 
