26c5a0412fa88fd248b75a07ae7c8c12d816a732
rmisu <roger889@umn.edu>
2022-06-09 04:40:01 -0500

    first functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?")
+// Note the lack of quotation marks around playerName
+console.log("Our robot's name is" + playerName);
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+
+
+
